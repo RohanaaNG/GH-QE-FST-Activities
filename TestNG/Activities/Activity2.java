e789b3010a00083dadee4b50fc6f8840d5ae058c
RohanaaNG <137982289+RohanaaNG@users.noreply.github.com>
2026-03-16 16:22:05 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity2.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import com.junit.BankAccount;
+import com.junit.NotEnoughFundsException;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Activity2 {
+
+    @Test
+    void notEnoughFunds() {
+
+        BankAccount account = new BankAccount(9);
+
+        assertThrows(NotEnoughFundsException.class,
+                () -> account.withdraw(10),
+                "Balance must be greater than amount of withdrawal");
+    }
+
+    @Test
+    void enoughFunds() {
+
+        BankAccount account = new BankAccount(100);
+
+        assertDoesNotThrow(() -> account.withdraw(100));
+    }
+}
